b6cd9079ae32b67e7469c0579856c703f41c0939
SIVANESANKAVITHAJINY <137502792+SIVANESANKAVITHAJINY@users.noreply.github.com>
2023-07-03 09:20:16 +0530

    Add files via upload

--- scrools 7[kavithajiny].py
+++ scrools 7[kavithajiny].py
@@ -3,6 +3,7 @@ import turtle
 from itertools import cycle
 colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
+
 def draw_circle(size, angle, shift):
     turtle.bgcolor(next(colors))
     turtle.pencolor(next(colors))
@@ -11,7 +12,8 @@ def draw_circle(size, angle, shift):
     turtle.forward(shift)
     draw_circle(size+5, angle+1, shift+1)
 
-turtle.bgcolor('green')
+
+turtle.bgcolor('pink')
 turtle.speed('fast')
 turtle.pensize(40)
 draw_circle(30, 0, 1)
